b014658065623da41b37d561456f8a522b4c0aa9
Margarita <m.novolokova@gmail.com>
2022-11-11 15:26:31 +0200

    Task 3

--- index.js
+++ index.js
@@ -7,21 +7,21 @@
 // виконати 2 варіантами: через switch та if...else if...else
 
 function checkSeasons() {
-  const lang = prompt();
+  const inputTime = prompt();
 
-  if (lang === "1" || lang === "2" || lang === "12") {
+  if (inputTime === "1" || inputTime === "2" || inputTime === "12") {
     console.log("winter");
-  } else if (lang === "3" || lang === "4" || lang === "5") {
+  } else if (inputTime === "3" || inputTime === "4" || inputTime === "5") {
     console.log("spring");
-  } else if (lang === "6" || lang === "7" || lang === "18") {
+  } else if (inputTime === "6" || inputTime === "7" || inputTime === "18") {
     console.log("summer");
-  } else if (lang === "9" || lang === "10" || lang === "11") {
+  } else if (inputTime === "9" || inputTime === "10" || inputTime === "11") {
     console.log("fall");
   } else {
     console.log("404 try again");
   }
 
-  switch (lang) {
+  switch (inputTime) {
     case "1":
     case "2":
     case "12":
@@ -47,7 +47,10 @@ function checkSeasons() {
       break;
   }
 }
+
 //checkSeasons();
+
+
 
 // Людина вводить вік (привести до числа), ви виводите класифікацію за віком:
 // [0 - 2) baby
@@ -85,4 +88,71 @@ function checkAge() {
   }
 }
 
-checkAge();
+//checkAge();
+
+
+
+// Людина вводить годину([0-23]), ви виводите привітання
+// [7 - 11) - good morning
+// [11 - 18) - good day
+// [18 - 23) - good evening
+// усе інше - good night
+// усе невірне - 404 try again
+// виконати 2 варіантами: через switch та if...else if...else і скажете який краще!!!
+
+function checkTime() {
+  const inputUser = prompt();
+  const inputTime = Number(inputUser);
+
+  if (inputTime >= 7 && inputTime < 11) {
+    console.log("good morning");
+    //console.log(inputTime + inputTime);
+  } else if (inputTime >= 11 && inputTime < 18) {
+    console.log("good day");
+  } else if (inputTime >= 18 && inputTime < 23) {
+    console.log("good evening");
+  } else if (inputTime > 0 && inputTime < 7) {
+    console.log("good night");
+  } else {
+    console.log("404 try again");
+  }
+
+  switch (inputTime) {
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      console.log("good morning");
+      break;
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+      console.log("good day");
+      break;
+    case 18:
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+      console.log("good evening");
+      break;
+    case 23:
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      console.log("good night");
+      break;
+    default:
+      console.log("404 try again");
+      break;
+  }
+}
+checkTime();
